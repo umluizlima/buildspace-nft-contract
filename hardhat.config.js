0a329736a22adc6cff32f4c1bf4c9d1ce96009f8
Luiz Lima <umluizlima@gmail.com>
2021-12-12 17:44:39 -0300

    Add etherscan to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require("@nomiclabs/hardhat-etherscan");
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
@@ -29,5 +30,8 @@ module.exports = {
       url: process.env.PROD_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
